a5750400c196eba51c7c8cc7fbb648944bba87e3
Shrestho <sadrilaminshrestho@gmail.com>
2021-12-04 17:54:25 +0600

    another_stuff_changed

--- UVA/1000/100/01.py
+++ UVA/1000/100/01.py
@@ -4,5 +4,14 @@
 
 
 n = 22
+result = []
 while True:
-    pass
+    result.append(n)
+    if n==1: break
+    if n % 2 == 1:
+        n = 3*n +1
+    else:
+        n //= 2
+    
+
+print(result.__len__())
